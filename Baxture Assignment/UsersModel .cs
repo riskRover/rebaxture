f29aea0c1f4c3868352a76e1607fc05d657425b3
Mayur Patil <mayurpatil1727@gmail.com>
2024-01-18 12:15:08 +0530

    first commit , created project and added model and properties in it , created Db and created table in it

---
+++ Baxture Assignment/UsersModel .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Baxture_Assignment
+{
+    public class UsersModel
+    {
+        public string Id { get; set; } 
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d).{6,}$", ErrorMessage = "Password must be alphanumeric and at least 6 characters long.")]
+        public string Password { get; set; }
+
+        public bool IsAdmin { get; set; } = false;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public List<string> Hobbies { get; set; } = new List<string>();
+    }
+}
+
