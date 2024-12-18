9586fb4cbe512c5c9eb4b2c6f4f58d475387b49a
riskRover <mr.sp3799@gmail.com>
2024-12-18 12:57:58 +0530

    Add Docker Compose  (#1)
    * Create docker-compose.yml
    
    * Update Dockerfile
    
    * Update Baxture Assignment.csproj
    
    * Update script.sql
    
    * Update script.sql
    
    * Update script.sql
    
    * Update lauch url
    
    Update launchSettings.json
    
    * Update Server name to Docker container name
    
    Update appsettings.json
    
    * Update Mysql root password
    
    * Update Database name
    
    * Update script.sql

--- Baxture Assignment/Docker-File/DB/script.sql
+++ Baxture Assignment/Docker-File/DB/script.sql
@@ -1,132 +1,187 @@
-USE `BaxtureAssignmentDB`;
- 
-/* SQLINES DEMO *** le [dbo].[Users]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
--- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
-CREATE TABLE `Users`(
-	`Id` int NULL,
-	`Username` varchar(50) NULL,
-	`Password` varchar(50) NULL,
-	`IsAdmin` Tinyint NULL,
-	`Age` int NULL,
-	`Hobbies` nvarchar(50) NULL
-);
-/* SQLINES DEMO *** redProcedure [dbo].[DeleteUserByID]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
-DELIMITER //
+CREATE DATABASE  IF NOT EXISTS `baxtureassignmentdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE USER IF NOT EXISTS 'root'@'Database' IDENTIFIED BY 'admin';
+-- Grant privileges to root user
+GRANT ALL PRIVILEGES ON `baxtureassignmentdb`.* TO 'root'@'Database';
+FLUSH PRIVILEGES;
+USE `baxtureassignmentdb`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: baxtureassignmentdb
+-- ------------------------------------------------------
+-- Server version       9.1.0
 
-CREATE PROCEDURE `DeleteUserByID` (
-    Id INT)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(50) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL,
+  `IsAdmin` tinyint(1) DEFAULT NULL,
+  `Age` int DEFAULT NULL,
+  `Hobbies` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'baxturedb'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `DeleteUserByID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `DeleteUserByID`(
+    IN `Id` INT
+)
 BEGIN
-    Delete from Users
-    WHERE Id = Id;
-END;
-//
-
+    DELETE FROM `users`
+    WHERE `Id` = Id;
+END ;;
 DELIMITER ;
-
-
-/* SQLINES DEMO *** redProcedure [dbo].[GetAllUsers]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
-
-
-delimiter //
-
-create procedure `GetAllUsers`()
-
-
-begin 
-      SELECT * FROM Users;
-end;
-//
-
-delimiter ;
-
-
-/* SQLINES DEMO *** redProcedure [dbo].[GetUserByID]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
-
-
-delimiter //
-
-create procedure `GetUserByID` (
-
-Id int) 
-
-
-begin 
-     select * from Users where Id = Id;
-end;
-//
-
-delimiter ;
-
-
-/* SQLINES DEMO *** redProcedure [dbo].[InsertUser]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
-delimiter //
-
-create procedure `InsertUser` (
-
-Id int ,
-Username varchar(50),
-Password varchar(50) ,
-IsAdmin tinyint ,
-Age int ,
-Hobbies varchar(50)) 
-
-
-begin 
-    insert into Users (Id  , Username , Password, IsAdmin , Age , Hobbies)
-	values (Id  , Username , Password, IsAdmin , Age , Hobbies);
-end;
-//
-
-delimiter ;
-
-
-/* SQLINES DEMO *** redProcedure [dbo].[UpdateUserByID]    Script Date: 11-12-2024 19:55:41 ******/
-/* SET ANSI_NULLS ON */
- 
-/* SET QUOTED_IDENTIFIER ON */
- 
-DELIMITER //
-
-CREATE PROCEDURE `UpdateUserByID` (
-    Id INT,
-    Username VARCHAR(50),
-    Password VARCHAR(50),
-    IsAdmin TINYINT,
-    Age INT,
-    Hobbies VARCHAR(50))
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetAllUsers` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `GetAllUsers`()
 BEGIN
-    UPDATE Users
+    SELECT * FROM `users`;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetUserByID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `GetUserByID`(
+    IN `Id` INT
+)
+BEGIN
+    SELECT * FROM `users` WHERE `Id` = Id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `InsertUser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `InsertUser`(
+    IN `Id` INT,
+    IN `Username` VARCHAR(50),
+    IN `Password` VARCHAR(50),
+    IN `IsAdmin` BOOLEAN,
+    IN `Age` INT,
+    IN `Hobbies` VARCHAR(50)
+)
+BEGIN
+    INSERT INTO `users` (`Id`, `Username`, `Password`, `IsAdmin`, `Age`, `Hobbies`)
+    VALUES (Id, Username, Password, IsAdmin, Age, Hobbies);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UpdateUserByID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `UpdateUserByID`(
+    IN `Id` INT,
+    IN `Username` VARCHAR(50),
+    IN `Password` VARCHAR(50),
+    IN `IsAdmin` BOOLEAN,
+    IN `Age` INT,
+    IN `Hobbies` VARCHAR(50)
+)
+BEGIN
+    UPDATE `users`
     SET
-        Username = Username,
-        Password = Password,
-        IsAdmin = IsAdmin,
-        Age = Age,
-        Hobbies = Hobbies
+        `Username` = Username,
+        `Password` = Password,
+        `IsAdmin` = IsAdmin,
+        `Age` = Age,
+        `Hobbies` = Hobbies
     WHERE
-        Id = Id;
-END;
-//
-
+        `Id` = Id;
+END ;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2024-12-11 20:20:38
